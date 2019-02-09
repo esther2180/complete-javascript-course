359465c3cc827bef9f7ad54e42a736fa483d3aea
Esther <esther2180@gmail.com>
2019-02-09 15:14:23 -0800

    Updating the Percentages Controller

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -215,6 +215,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var setupEventListeners = function() {
         var DOM = UICtrl.getDOMstrings();
+
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
         document.addEventListener('keypress', function(event) {
@@ -235,7 +236,17 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 3. Display the budget on the UI
         UICtrl.displayBudget(budget);
-    }
+    };
+
+    var updatePercentages = function () {
+
+        // 1. Calculate percentages
+
+        // 2. Read percentages from the budget controller
+
+        // 3. Update the UI with the new percentages
+
+    };
 
     var ctrlAddItem = function() {
         var input, newItem;
@@ -255,6 +266,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 5. Calculate and update budget
             updateBudget();
+
+            // 6. Calculate and update percentages
+            updatePercentages();
         }    
     };
 
@@ -277,6 +291,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 3. Update and show the new budget
             updateBudget();
+
+             // 4. Calculate and update percentages
+             updatePercentages();
         }
     };
 
