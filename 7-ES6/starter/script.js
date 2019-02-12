99db3adac3854018183762812b5d69f9bb601c91
Esther <esther2180@gmail.com>
2019-02-11 18:10:58 -0800

    ES6 Arrow Functions

---
+++ 7-ES6/starter/script.js
@@ -0,0 +1,199 @@
+// Lecture: let and const
+
+// ES5
+// var name5 = 'Jane Smith';
+// var age5 = 23;
+// name5 = 'Jane Miller';
+// console.log(name5);
+
+// ES6
+// const name6 = 'Jane Smith';
+// let age6 = 23;
+// name6 = 'Jane Miller';
+// console.log(name6);
+
+// ES5
+// function driverLicense(passedTest) {
+
+//     if (passedTest) {
+//         var firstName = 'John';
+//         var yearOfBirth = 1990;
+//     }
+//     console.log(firstName + ', born in ' + yearOfBirth + ' , is now officially allowed to drive a car.');
+// }
+
+// driverLicense(true);
+
+
+// ES6
+// function driverLicense(passedTest) {
+
+//     let firstName = 'John';
+//     const yearOfBirth = 1990;
+
+//     if (passedTest) {
+//         firstName = 'John';
+//     }
+//     console.log(firstName + ', born in ' + yearOfBirth + ' , is now officially allowed to drive a car.');
+// }
+// driverLicense(true);
+
+
+
+
+// let i = 23;
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+
+
+
+////////////////////////////////
+// Lexture: Blocks and IIFEs
+
+// {
+//     const a = 1;
+//     let b = 2;
+//     var c = 3;
+//     console.log(a + b);
+// }
+
+// ES5
+// (function() {
+//     var c = 3;
+// })();
+// console.log(c);
+
+
+
+
+////////////////////////////////
+// Lexture: Strings
+
+// let firstName = 'John';
+// let lastName = 'Smith';
+// const yearOfBirth = 1990;
+
+// function calcAge(year) {
+//     return 2016 - year;
+// }
+
+//ES5 
+// console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
+
+//ES6
+// console.log(`This is ${firstName} ${lastName}. He was bron in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+// const n = `${firstName} ${lastName}`;
+// console.log(n.startsWith('j'));
+// console.log(n.endsWith('th'));
+// console.log(n.includes('oh'));
+// console.log(`${firstName} `.repeat(5));
+
+
+
+
+
+////////////////////////////////
+// Lexture: Arrow Functions 1
+
+// const years = [1990, 1965, 1982, 1937];
+
+// // ES5
+// var ages5 = years.map(function(el) {
+//     return 2016 - el;
+// });
+// console.log(ages5);
+
+
+// ES6 
+// let ages6 = years.map(el => 2016 - el);
+// console.log(ages6);
+
+// ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+// console.log(ages6);
+
+// ages6 = years.map((el, index) => {
+//     const now = new 
+//     Date().getFullYear();
+//     const age = now - el;
+//     return `Age element ${index + 1}: ${age}.`
+// });
+// console.log(ages6);
+
+
+
+////////////////////////////////
+// Lexture: Arrow Functions 2
+
+// ES5
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+
+        var self = this;
+        document.querySelector('.green').addEventListener('click', function() {
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+}
+// box5.clickMe();
+
+
+
+// ES6
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+// box6.clickMe();
+
+// const box66 = {
+//     color: 'green',
+//     position: 1,
+//     clickMe: () => {
+
+//         document.querySelector('.green').addEventListener('click', () => {
+//             var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+//             alert(str);
+//         });
+//     }
+// }
+// box66.clickMe();
+
+
+function Person(name) {
+    this.name = name;
+}
+
+// ES5
+Person.prototype.myFriends5 = function(friends) {
+    var arr = friends.map(function(el) {
+        return this.name + ' is friends with ' + el;
+    }.bind(this));
+    console.log(arr);
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
+
+
+// ES6
+Person.prototype.myFriends5 = function(friends) {
+    var arr = friends.map(el => `${this.name} is friends with ${el}`);
+    console.log(arr);
+}
+
+new Person('Mike').myFriends5(friends);
