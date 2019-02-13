9cd9afed12119b1b8b1016b0d40e6461ba469cf8
Esther <esther2180@gmail.com>
2019-02-12 22:07:24 -0800

    Arrays in ES6

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -208,29 +208,80 @@
 // var age = john[1];
 
 // ES6
-const [name, age] = ['John', 26];
-console.log(name);
-console.log(age);
+// const [name, age] = ['John', 26];
+// console.log(name);
+// console.log(age);
 
-const obj = {
-    firstName: 'John',
-    lastName: 'Smith'
-};
+// const obj = {
+//     firstName: 'John',
+//     lastName: 'Smith'
+// };
 // const {firstName, lastName} = obj;
 // console.log(firstName);
 // console.log(lastName);
 
-const {firstName: a, lastName: b} = obj;
-console.log(a);
-console.log(b);
+// const {firstName: a, lastName: b} = obj;
+// console.log(a);
+// console.log(b);
 
 
-function calcAgeRetirement(year) {
-    const age = new Date().getFullYear() - year;
-    return [age, 65 - age];
+// function calcAgeRetirement(year) {
+//     const age = new Date().getFullYear() - year;
+//     return [age, 65 - age];
+// }
+
+// const [age2, retirement] = 
+// calcAgeRetirement(1990);
+// console.log(age2);
+// console.log(retirement);
+
+
+
+
+////////////////////////////////
+// Lecture: Arrays
+
+const boxes = document.querySelectorAll('.box');
+
+//ES5
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(cur) {
+    cur.style.backgroundColor = 'dodgerblue';
+});
+
+//ES6
+Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerBlue');
+
+
+//ES5
+// for(var i = 0; i < boxesArr5.length; i++) {
+//     if (boxesArr5[i].className === 'box blue') {
+//         continue;
+//     }
+//     boxesArr5[i].textContent = 'I changed to blue!';
+// }
+
+//ES6 
+const boxesArr6 = Array.from(boxes);
+for (const cur of boxesArr6) {
+    if (cur.className.includes('blue')) {
+        continue;
+    }
+    cur.textContent = 'I changed AGAIN to blue!'
 }
 
-const [age2, retirement] = 
-calcAgeRetirement(1990);
-console.log(age2);
-console.log(retirement);
+
+//ES5 
+var ages = [12, 17, 8, 21, 14, 11];
+
+// var full = ages.map(function(cur) {
+//     return cur >= 18;
+// });
+// console.log(full);
+
+// console.log(full.indexOf(true));
+// console.log(ages[full.indexOf(true)]);
+
+//ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
