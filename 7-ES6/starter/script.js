bc3257bfa148a781259588951449014f8b03d443
Esther <esther2180@gmail.com>
2019-03-05 19:21:45 -0800

    Advance ES6 Finished

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -410,17 +410,17 @@
 ////////////////////////////////
 // Lecture: Maps
 
-const question = new Map();
-question.set('question', 'What is the official name of the latest major Javascript version?');
-question.set(1, 'ES5');
-question.set(2, 'ES6');
-question.set(3, 'ES2015');
-question.set(4, 'ES7');
-question.set('correct', 3);
-question.set(true, 'Correct answer');
-question.set(false, 'Wrong, please try again');
+// const question = new Map();
+// question.set('question', 'What is the official name of the latest major Javascript version?');
+// question.set(1, 'ES5');
+// question.set(2, 'ES6');
+// question.set(3, 'ES2015');
+// question.set(4, 'ES7');
+// question.set('correct', 3);
+// question.set(true, 'Correct answer');
+// question.set(false, 'Wrong, please try again');
 
-console.log(question.get('question'));
+// console.log(question.get('question'));
 // console.log(question.size);
 
 // if(question.has(4)) {
@@ -432,14 +432,14 @@ console.log(question.get('question'));
 
 // question.forEach((value, key) => console.log(`This is ${key}, and it's set to ${value}`));
 
-for (let [key, value] of question.entries()) {
-    if (typeof(key) === 'number') {
-        console.log(`Answer ${key}: ${value}`);
-    }
-}
+// for (let [key, value] of question.entries()) {
+//     if (typeof(key) === 'number') {
+//         console.log(`Answer ${key}: ${value}`);
+//     }
+// }
 
-const ans = parseInt(prompt('Write the correct answer'));
-console.log(question.get(ans === question.get('correct')));
+// const ans = parseInt(prompt('Write the correct answer'));
+// console.log(question.get(ans === question.get('correct')));
 
 
 ////////////////////////////////
@@ -504,7 +504,7 @@ console.log(question.get(ans === question.get('correct')));
 //     this.medals = medals;
 // }
 
-// Athlete5.prototype = Object.create(Person5.prototype);
+// Athlete5.prototype = Object.create(Person5.prototype); //connecting 2 prototype chains
 
 // Athlete5.prototype.wonMedal = function() {
 //     this.medals++;
@@ -554,7 +554,7 @@ console.log(question.get(ans === question.get('correct')));
 
 ////////////////////////////////
 // CODING CHALLENGE
-/*
+
 class Element {
     constructor(name, buildYear) {
         this.name = name;
@@ -642,4 +642,3 @@ function reportStreets(s) {
 
 reportParks(allParks);
 reportStreets(allStreets);
-*/
