f0fe67fab76c432dbf5a5b66cd4de27b0d025260
Esther <esther2180@gmail.com>
2019-03-11 15:24:45 -0700

    Shopping List Controller

--- 9-forkify/starter/src/js/config.js
+++ 9-forkify/starter/src/js/config.js
@@ -1,3 +1,3 @@
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
-export const key = 'd2b7449ed5080b93393fc56f0e28d807';
-// export const key = '79c6f39fbe0e91d376170e6fa445baee';
+// export const key = 'd2b7449ed5080b93393fc56f0e28d807';
+export const key = '79c6f39fbe0e91d376170e6fa445baee';
